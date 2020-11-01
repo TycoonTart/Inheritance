d70d8d579e138e1e0089eef5834b619e116dac19
TycoonTart <72146197+TycoonTart@users.noreply.github.com>
2020-11-01 18:51:18 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,11 @@
+class log extends BaseClass{
+    constructor(x,y,height,angle){
+        super(x,y,20,height,angle)
+        Matter.Body.setAngle(this.body,angle)
+       this.image=loadImage("wood2.png");
+
+    }
+
+    
+    
+}
